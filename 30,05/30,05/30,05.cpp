f201becd4992ce0f896fd84eb6d68038b9113d1d
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2024-06-03 21:15:48 +0300

    Add files via upload

---
+++ 30,05/30,05/30,05.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+using namespace std;
+
+class WaterSupply
+{
+public:
+    void FillWater()
+    {
+        cout << "Filling water\n";
+    }
+
+    void DrainWater()
+    {
+        cout << "Draining water\n";
+    }
+};
+
+class DetergentDispenser
+{
+public:
+    void AddDetergent()
+    {
+        cout << "Adding detergent\n";
+    }
+};
+
+class Washer
+{
+public:
+    void StartWashing()
+    {
+        cout << "Starting washing\n";
+    }
+
+    void StopWashing()
+    {
+        cout << "Stopping washing\n";
+    }
+};
+
+class Dryer
+{
+public:
+    void StartDrying()
+    {
+        cout << "Starting drying\n";
+    }
+
+    void StopDrying()
+    {
+        cout << "Stopping drying\n";
+    }
+};
+
+class WashingMachineFacade
+{
+public:
+    WashingMachineFacade()
+        : waterSupply(new WaterSupply()), detergentDispenser(new DetergentDispenser()),
+        washer(new Washer()), dryer(new Dryer()) {}
+
+    ~WashingMachineFacade()
+    {
+        delete waterSupply;
+        delete detergentDispenser;
+        delete washer;
+        delete dryer;
+    }
+
+    void StartFullWashCycle()
+    {
+        waterSupply->FillWater();
+        detergentDispenser->AddDetergent();
+        washer->StartWashing();
+        washer->StopWashing();
+        waterSupply->DrainWater();
+        dryer->StartDrying();
+        dryer->StopDrying();
+    }
+
+private:
+    WaterSupply* waterSupply;
+    DetergentDispenser* detergentDispenser;
+    Washer* washer;
+    Dryer* dryer;
+};
+
+int main()
+{
+    WashingMachineFacade washingMachine;
+    washingMachine.StartFullWashCycle();
+    return 0;
+}
